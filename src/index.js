2fb21410d93692983a762f41ade67eb01503684c
Diego <[diego.carcaustor@gmail.com]>
2022-02-08 22:34:16 -0500

    Se agrega el proyecto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-
-import {saludar} from './js/componentes.js';
-// por mas de que no coloque la terminacion .js, siempre buscará un archivo
-// que tenga esa extensión.
 import './styles.css';
+import {generarPoster} from './js/componentes';
 
-const nombre= 'Diego';
+const contenedor= document.querySelector(".flex-container");
 
-saludar(nombre);
-// se pueden utilizar librerias de otras personas en mis propios proyectos.
-// React y angular ya tienen incorporado el webpack por defecto.
+let documentFragment = document.createDocumentFragment();
+let puntosTotales=0;
+for(let i=1; i<=7; i++){
+    let coleccionRandom=Math.round(Math.random()*10000);
+    let fanpointsRandom= Math.round(Math.random()*100+41);
+    let poster=generarPoster("",`Poster ${i}`, `coleccion ${coleccionRandom}`,fanpointsRandom);
+    let div=document.createElement('div');
+    //let divFanpoints=document.createElement('span');
+
+    div.addEventListener('click',()=>{
+        document.querySelector('.key-data').value=coleccionRandom
+        //console.log(fanpointsRandom);
+        puntosTotales+=fanpointsRandom;
+        console.log(puntosTotales);
+        //document.querySelector('.todo-count').value=puntosTotales;
+        const etiquetaPuntos= document.querySelector('.todo-count');
+        etiquetaPuntos.innerText=`Fanpoints totales: ${puntosTotales}`
+        
+    });
+    div.tabIndex=i;
+    div.classList.add(`item-${i}`,'flex-item');
+    div.innerHTML=poster.join(" ");
+    documentFragment.appendChild(div);
+
+}
+contenedor.appendChild(documentFragment);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,17 +1,7 @@
-import '../css/componentes.css';
-//import webpacklogo from '../assets/img/webpack-logo.png';
-
-export const saludar= (nombre)=>{
-    console.log('Creando etiquetas h1');
-
-    const h1= document.createElement('h1');
-    h1.innerText= `Hola, ${nombre}`;
-
-    document.body.append(h1);
-
-
-    // Img
-    /*const img= document.createElement('img');
-    img.src= webpacklogo;
-    document.body.append(img);*/
+export function generarPoster(img,nombre,coleccion,fanpoints){
+	img = "<img class='poster-img' src='https://res.cloudinary.com/dby5zzxin/image/upload/v1644257431/hbfk5z5hqjsdgac67osb.jpg'>";
+	nombre = `<h2>${nombre}</h2>`;
+	coleccion = `<h3>${coleccion}</h3>`;
+	fanpoints = `<p>Fanpoints: <b>${fanpoints}</b></p>`;
+	return [img,nombre,coleccion,fanpoints];
 }
